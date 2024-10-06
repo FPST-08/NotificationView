0bd9922ef4f412d541d43f3c31c6ab3e523d256a
Aditya Saravana <aditya.saravana@icloud.com>
2024-10-05 17:03:38 -0700

    fixed indentation

--- Sources/NotificationView/NotificationView.swift
+++ Sources/NotificationView/NotificationView.swift
@@ -8,9 +8,9 @@ import Foundation
 public struct NotificationView: View {
     func appIcon(in bundle: Bundle = .main) -> String {
         guard let icons = bundle.object(forInfoDictionaryKey: "CFBundleIcons") as? [String: Any],
-              let primaryIcon = icons["CFBundlePrimaryIcon"] as? [String: Any],
-              let iconFiles = primaryIcon["CFBundleIconFiles"] as? [String],
-              let iconFileName = iconFiles.last else {
+            let primaryIcon = icons["CFBundlePrimaryIcon"] as? [String: Any],
+            let iconFiles = primaryIcon["CFBundleIconFiles"] as? [String],
+            let iconFileName = iconFiles.last else {
             fatalError("Could not find icons in bundle")
         }
         
